3e2dbc11a98ff0bdf566dff30961f29a521c46f9
Jake W. Ireland <jakewilliami@icloud.com>
2021-01-18 12:49:15 +1300

    Added _some_ generated `.jl` files from `.proto` files
    In order of adding:
      - `src/generated/protobuf/concept.proto`
      - `src/generated/protobuf/database.proto`
      - `src/generated/protobuf/logic.proto`
      - `src/generated/protobuf/options.proto`
    
    Still missing:
      - `src/generated/protobuf/answer.proto`
      - `src/generated/protobuf/query.proto`
      - `src/generated/protobuf/transaction.proto`
      - `src/generated/protobuf/grakn.proto`
      - `src/generated/protobuf/session.proto`
    
    Usually the errors for the ones that have not been generated yet are import errors: "Can't find `protobuf/concept` or invalid".  I am unsure why we are still getting those errors.

--- src/generated/grakn.jl
+++ src/generated/grakn.jl
@@ -2,6 +2,6 @@ module grakn
   const _ProtoBuf_Top_ = @static isdefined(parentmodule(@__MODULE__), :_ProtoBuf_Top_) ? (parentmodule(@__MODULE__))._ProtoBuf_Top_ : parentmodule(@__MODULE__)
   module protocol
     const _ProtoBuf_Top_ = @static isdefined(parentmodule(@__MODULE__), :_ProtoBuf_Top_) ? (parentmodule(@__MODULE__))._ProtoBuf_Top_ : parentmodule(@__MODULE__)
-    include("concept_pb.jl")
+    include("options_pb.jl")
   end
 end
