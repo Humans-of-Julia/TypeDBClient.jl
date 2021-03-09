b29e7601037e10e08c1bdbf4dc65f43683f31bf1
Frank Urbach <frank.urbach@gmx.de>
2021-03-09 02:09:18 +0100

    first attempt of fully implementd Session

--- src/specific_transaction_experiment.jl
+++ src/specific_transaction_experiment.jl
@@ -11,4 +11,10 @@ opt = GraknClient.GraknOptions(5000)
 
 ses = GraknClient.Session(cli, "test", opt, GraknClient.Session_Type[:SCHEMA]) 
 
+ses._scheduler.session_run = false
+
 tra = GraknClient.Transaction(ses, GraknClient.Transaction_Type[:READ], opt)
+
+GraknClient.close(ses)
+
+ses._scheduler.session_run = false
