7f1f17bcf90068a224f6f81aa128f14078525838
Frank Urbach <frank.urbach@gmx.de>
2021-04-27 23:47:48 +0200

    building a separate file for precompile

---
+++ src/precompile.jl
@@ -0,0 +1,14 @@
+########## precompiling section
+
+@assert precompile(meta, (Type{Proto.Type_Res}, ))
+@assert precompile(meta, (Type{Proto.Type_Req}, ))
+@assert precompile(meta, (Type{Proto.Transaction_Res}, ))
+@assert precompile(meta, (Type{Proto.Transaction_ResPart}, ))
+
+@assert precompile(which_oneof, (Proto.Transaction_ResPart, Symbol))
+@assert precompile(which_oneof, (Proto.Transaction_Res, Symbol))
+@assert precompile(single_request, (BidirectionalStream, Proto.Transaction_Req, Bool))
+@assert precompile(_open_result_channel, (BidirectionalStream, Proto.Transaction_Req, Bool))
+@assert precompile(collect_result, (Channel{Union{Proto.Transaction_Res,Proto.Transaction_ResPart}}, ))
+@assert precompile(_is_stream_respart_done, (Proto.Transaction_Res, ))
+@assert precompile(_is_stream_respart_done, (Proto.Transaction_ResPart, ))
