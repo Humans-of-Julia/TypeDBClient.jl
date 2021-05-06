b73f7e1ce650a63cc2f321862672bdb2b806d784
Frank Urbach <frank.urbach@gmx.de>
2021-05-06 20:27:27 +0200

    Experiment with grpc-Status

--- src/core/CoreTransaction.jl
+++ src/core/CoreTransaction.jl
@@ -27,18 +27,16 @@ function CoreTransaction(session::CoreSession ,
 
     grpc_controller = gRPCController(request_timeout=request_timout)
 
-    req_result, status = transaction(session.client.core_stub.blockingStub, grpc_controller, input_channel)
-    output_channel = grpc_result_or_error(req_result, status, result->result)
+    output_channel, status = transaction(session.client.core_stub.blockingStub, grpc_controller, input_channel)
+    grpc_result_or_error(output_channel, status, result->result)
 
     open_req = TransactionRequestBuilder.open_req(session.sessionID, type, proto_options,session.networkLatencyMillis)
 
-    bidirectionalStream = BidirectionalStream(input_channel, output_channel)
+    bidirectionalStream = BidirectionalStream(input_channel, output_channel, status)
     trans_id = uuid4()
     result = CoreTransaction(type, options, bidirectionalStream, trans_id, sessionId, request_timout)
 
     req_result = execute(result, open_req, false)
-    kind_of_result = which_oneof(req_result, :res)
-    open_req_res = getproperty(req_result, kind_of_result)
 
     return result
 end
