9375aa503d43928c401729ef227d00bfc907c2a8
Frank Urbach <frank.urbach@gmx.de>
2022-03-02 20:37:48 +0100

    Changed the precompiling directives

--- src/precompile/precompile_ProtoBuf.jl
+++ src/precompile/precompile_ProtoBuf.jl
@@ -14,9 +14,9 @@ function _precompile_()
     @assert Base.precompile(Tuple{typeof(ProtoBuf.wiretype),Type{Vector{UInt8}}})   # time: 0.004197969
     @assert Base.precompile(Tuple{typeof(ProtoBuf.wiretype),Type{Float64}})   # time: 0.004151102
     @assert Base.precompile(Tuple{typeof(ProtoBuf.wiretype),Type{Dict{AbstractString, AbstractString}}})   # time: 0.004144187
-    @assert Base.precompile(Tuple{typeof(ProtoBuf.writeproto),IOBuffer,Vector{UInt8},ProtoMetaAttribs})   # time: 0.003951
+#    @assert Base.precompile(Tuple{typeof(ProtoBuf.writeproto),IOBuffer,Vector{UInt8},ProtoMetaAttribs})   # time: 0.003951
     @assert Base.precompile(Tuple{typeof(ProtoBuf.defaultval),Type{Int64}})   # time: 0.003861888
-    @assert Base.precompile(Tuple{typeof(ProtoBuf.writeproto),IOBuffer,Int32,ProtoMetaAttribs})   # time: 0.002705993
+#    @assert Base.precompile(Tuple{typeof(ProtoBuf.writeproto),IOBuffer,Int32,ProtoMetaAttribs})   # time: 0.002705993
     @assert Base.precompile(Tuple{typeof(ProtoBuf.defaultval),Type{Vector{UInt8}}})   # time: 0.001826776
 
     @info "Precompiling ProtoBuf done!"
